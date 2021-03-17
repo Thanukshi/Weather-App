e33567290fe06f664552d3a8e5f711a6bf0bad3c
Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2021-03-17 13:38:16 +0530

    Commited

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 
-const express = required('express');
+const express = require('express');
 
 const app = express();
 
-const bodyParser = required ('body-parser');
+const bodyParser = require('body-parser');
+
+const PORT = 5000; 
+
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.json());
+
+const router = require('./router');
+
+app.use("/forecasts/", router);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const d = new Date();
+    res.json({currentTime: d.toTimeString()});
+    console.log('Received a GET request');
+});
+
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Server is listening on port ${PORT}...`);
+});
